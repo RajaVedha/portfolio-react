243023cf30c279604e05fa288a2a8d97b5a274b4
raja vedha <bandiraja2018@gmail.com>
2023-08-06 19:25:07 +0530

    Updates

---
+++ precache-manifest.ab425cff71c63618a99f0d8a7ea7b4c4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9704beeb34940b34738",
+    "url": "/portfolio-react/static/css/main.49d03453.chunk.css"
+  },
+  {
+    "revision": "e9704beeb34940b34738",
+    "url": "/portfolio-react/static/js/main.e9704bee.chunk.js"
+  },
+  {
+    "revision": "f13e2d7f7123de3ff015",
+    "url": "/portfolio-react/static/js/1.f13e2d7f.chunk.js"
+  },
+  {
+    "revision": "d2b996e9b7613c26d026",
+    "url": "/portfolio-react/static/css/2.ac4e82a4.chunk.css"
+  },
+  {
+    "revision": "d2b996e9b7613c26d026",
+    "url": "/portfolio-react/static/js/2.d2b996e9.chunk.js"
+  },
+  {
+    "revision": "579c1ba0181e567d5d86",
+    "url": "/portfolio-react/static/js/runtime~main.579c1ba0.js"
+  },
+  {
+    "revision": "90522605ba55dc15b5078f4561c516f2",
+    "url": "/portfolio-react/static/media/proj1.90522605.jpg"
+  },
+  {
+    "revision": "e3a7c6929fe5cbacc2ac3204ccdc3b7c",
+    "url": "/portfolio-react/static/media/proj2.e3a7c692.png"
+  },
+  {
+    "revision": "46570bf3024b9baf3278d5c86d06ef1a",
+    "url": "/portfolio-react/static/media/proj3.46570bf3.png"
+  },
+  {
+    "revision": "231371a6869b947ae2516a5c9da58f76",
+    "url": "/portfolio-react/static/media/proj4.231371a6.png"
+  },
+  {
+    "revision": "74ea82f048f29cb33fd5e39039fbcf54",
+    "url": "/portfolio-react/static/media/proj5.74ea82f0.png"
+  },
+  {
+    "revision": "3c09bbb638bd6aeb4fcb22a00c81f01e",
+    "url": "/portfolio-react/static/media/proj6.3c09bbb6.png"
+  },
+  {
+    "revision": "2dcedff5a72394cb69a25eb8ea8c570c",
+    "url": "/portfolio-react/index.html"
+  }
+];
