258347dbecf845693a6de572962b2226996d08f5
raja vedha <bandiraja2018@gmail.com>
2023-08-06 20:16:47 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
         <button onClick={() => {setExpandNavbar((prev) => !prev)}}> <ReorderIcon /> </button>
         </div>
        <div className='links'>
-        <Link to='/'>Home</Link>
+        <Link to='/portfolio-react/'>Home</Link>
         <Link to='/projects'>Projects</Link>
         <Link to='/certificates'>Certificates</Link>
         </div> 
